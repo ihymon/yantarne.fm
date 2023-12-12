1023ccf7dd7e0c8bf094cf012f457bcf6475eb93
Roman Artemiev <115183967+Roman-Artemiev@users.noreply.github.com>
2023-12-12 07:56:52 +0100

    project initialization & make base setting

---
+++ javascript/main.js
@@ -0,0 +1,27 @@
+// Home Start
+// Home End
+
+
+
+// Schedule Start
+// Schedule End
+
+
+
+// About Us Start
+// About Us End
+
+
+
+// Team Start
+// Team End
+
+
+
+// Partners Start
+// Partners End
+
+
+
+// Footer Start
+// Footer End
